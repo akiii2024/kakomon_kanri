f0051b094f07210c9c18091aae00d84330b1da05
akiii2024 <akisaki.420.816@gmail.com>
2024-06-10 13:29:11 +0900

    引っ張って更新

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -179,83 +179,88 @@ class _MyHomePageState extends State<MyHomePage> {
           : Column(
         children: <Widget>[
           Expanded(
-            child: ListView.builder(
-              itemCount: _pastEntries.length,
-              itemBuilder: (context, index) {
-                var imagePath = _pastEntries[index]['imagePath'];
-                return Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(width: 1.0, color: Colors.grey),
+            child: RefreshIndicator(
+              onRefresh: () async {
+                await _loadCloudFire();
+              },
+              child: ListView.builder(
+                itemCount: _pastEntries.length,
+                itemBuilder: (context, index) {
+                  var imagePath = _pastEntries[index]['imagePath'];
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(width: 1.0, color: Colors.grey),
+                      ),
                     ),
-                  ),
-                  child: ListTile(
-                    leading: imagePath != null  && imagePath.isNotEmpty
-                      ?(imagePath.startsWith('assets/images/')
-                        ? Image.asset(imagePath)
-                        : (File(imagePath).existsSync()
-                          ? Image.file(File(imagePath))
-                          : Image.asset('assets/images/Question-Mark-PNG-Transparent-Image.png')))
-                      : Image.asset('assets/images/Question-Mark-PNG-Transparent-Image.png'),
-                    title: Text('講師名：${_pastEntries[index]['teacherName']}'),
-                    subtitle: Text('授業名：${_pastEntries[index]['className']}'),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        if(Platform.isAndroid){
-                          showDialog(
-                        context: context,
-                        builder: (_) => AlertDialog(
-                          title: Text("確認"),
-                          content: Text("本当に削除してよろしいですか？"),
-                          actions:[
-                            TextButton(
-                              child: Text("いいえ"),
-                              onPressed: (){
-                                Navigator.of(context).pop();
-                              }),
+                    child: ListTile(
+                      leading: imagePath != null  && imagePath.isNotEmpty
+                        ?(imagePath.startsWith('assets/images/')
+                          ? Image.asset(imagePath)
+                          : (File(imagePath).existsSync()
+                            ? Image.file(File(imagePath))
+                            : Image.asset('assets/images/Question-Mark-PNG-Transparent-Image.png')))
+                        : Image.asset('assets/images/Question-Mark-PNG-Transparent-Image.png'),
+                      title: Text('講師名：${_pastEntries[index]['teacherName']}'),
+                      subtitle: Text('授業名：${_pastEntries[index]['className']}'),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          if(Platform.isAndroid){
+                            showDialog(
+                          context: context,
+                          builder: (_) => AlertDialog(
+                            title: Text("確認"),
+                            content: Text("本当に削除してよろしいですか？"),
+                            actions:[
                               TextButton(
-                                child: Text("はい"),
+                                child: Text("いいえ"),
                                 onPressed: (){
-                                  _deleteEntry(index);
                                   Navigator.of(context).pop();
-                                },
-                                )
-                          ],
-                        )
-                      );
-                        }else{
-                          showDialog(
-                            context: context,
-                             builder: (_) => CupertinoAlertDialog(
-                              title: Text("確認"),
-                              content: Text("本当に削除してよろしいですか？"),
-                              actions: [
-                                CupertinoDialogAction(
-                                  child: Text("いいえ"),
-                                  isDestructiveAction: false,
-                                  onPressed: (){
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                CupertinoDialogAction(
+                                }),
+                                TextButton(
                                   child: Text("はい"),
-                                  isDestructiveAction: true,
                                   onPressed: (){
                                     _deleteEntry(index);
                                     Navigator.of(context).pop();
                                   },
                                   )
-                              ],
-                            )
-                          );
+                            ],
+                          )
+                        );
+                          }else{
+                            showDialog(
+                              context: context,
+                               builder: (_) => CupertinoAlertDialog(
+                                title: Text("確認"),
+                                content: Text("本当に削除してよろしいですか？"),
+                                actions: [
+                                  CupertinoDialogAction(
+                                    child: Text("いいえ"),
+                                    isDestructiveAction: false,
+                                    onPressed: (){
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  CupertinoDialogAction(
+                                    child: Text("はい"),
+                                    isDestructiveAction: true,
+                                    onPressed: (){
+                                      _deleteEntry(index);
+                                      Navigator.of(context).pop();
+                                    },
+                                    )
+                                ],
+                              )
+                            );
+                          }
                         }
-                      }
+                      ),
+                      onTap: () => _onEntryTap(index),
                     ),
-                    onTap: () => _onEntryTap(index),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
         ],
